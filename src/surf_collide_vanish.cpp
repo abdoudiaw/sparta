f8efb7d0b5645157c24b89ab3034ef94a16a69d9
Abdou Diaw <diawa@ornl.gov>
2023-11-22 13:08:30 -0500

    all

--- src/surf_collide_vanish.cpp
+++ src/surf_collide_vanish.cpp
@@ -11,10 +11,11 @@
 
    See the README file in the top-level SPARTA directory.
 ------------------------------------------------------------------------- */
-
+#include <fstream> 
 #include "math.h"
 #include "surf_collide_vanish.h"
 #include "error.h"
+#include <iostream> // Include header for standard I/O operations
 
 using namespace SPARTA_NS;
 
@@ -40,8 +41,48 @@ SurfCollideVanish::SurfCollideVanish(SPARTA *sparta, int narg, char **arg) :
 Particle::OnePart *SurfCollideVanish::
 collide(Particle::OnePart *&ip, double &, int, double *, int, int &)
 {
+        Particle::Species *species = particle->species;
+
+  // exit(0);
   nsingle++;
 
+  // save particle info and surface normal
+
+  double x[3],v[3];
+  x[0] = ip->x[0];
+  x[1] = ip->x[1];
+  x[2] = ip->x[2];
+  v[0] = ip->v[0];
+  v[1] = ip->v[1];
+  v[2] = ip->v[2];
+  int isp = ip->ispecies;
+  double mass = species[isp].molwt; 
+  double charge = species[isp].charge;
+
+
+    // Open a file in append mode
+    std::ofstream file("particle_data.txt", std::ios::app);
+
+    // Check if the file is open and write data
+    if (file.is_open()) {
+        // Writing headers if the file is empty
+        file.seekp(0, std::ios::end); // Go to the end of file
+        if (file.tellp() == 0) { // Check if file is empty
+            file << "x,y,z,vx,vy,vz,charge,mass" << std::endl; // Write headers
+        }
+
+        // Write data in columns
+        file << x[0] << ',' << x[1] << ',' << x[2] << ','
+             << v[0] << ',' << v[1] << ',' << v[2] << ','
+             << charge << ',' << mass << std::endl;
+
+        // Close the file
+        file.close();
+    } else {
+        std::cerr << "Unable to open file for writing." << std::endl;
+    }
+
+// save 
   ip = NULL;
   return NULL;
 }
